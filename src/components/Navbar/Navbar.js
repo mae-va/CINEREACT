e117ad8c21c4ba15f267edaf9149ccd708fea2ae
Maeva Duran <maeva.v.duran@gmail.com>
2018-10-03 17:19:38 +0200

    indentation du code

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,51 +12,44 @@ class Navbar extends Component {
           <div className="container-fluid">
 
           
-            <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-                <a className="logo" href="index.html">
-                    <img className="logo-img" src="./logo_cine-react.png" alt=""/>
+            <nav className="navbar navbar-expand-lg fixed-top"> {/* Navbar du haut*/}
+                <a href="index.html">
+                    <img src="./logo_cine-react.png" alt="logo de CineReact"/> {/* Logo */}
                 </a>
 
-                
-                {/*<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-        </button>*/}
-                <div className="collapse navbar-collapse pr-5" id="navbarNav">
+                <div className="collapse navbar-collapse" > {/* icones de droite*/}
                   <div>
                     <button className="btn btn-link search" type="button" >
                           <i class="fa fa-search fa-2x"></i>
                     </button>
-                    </div>
+                  </div>
 
-                    <div>
-                      <ul className="navbar-nav">
-                          <li className="nav-item active pr-5">
-                              
-                              <a className="nav-link" href="#">
-                                <i className="fa fa-heart icon mt-1"></i>
-                                <p className="ml-3 mt-3">Favoris</p>
-                              </a>
-                          </li>
-                          <li className="nav-item">
-                              <a className="nav-link" href="#">
-                                <i className="fa fa-envelope icon mt-1"></i>
-                                <p className="ml-3 mt-3">Contact</p>
-                              </a>
-                          </li>
-
-                      </ul>
-                    </div>
+                  <div>
+                    <ul className="navbar-nav">
+                      <li className="nav-item active pr-5">
+                        <a className="nav-link" href="#">
+                          <i className="fa fa-heart icon mt-1"></i>
+                          <p className="ml-3 mt-3">Favoris</p>
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a className="nav-link" href="#">
+                          <i className="fa fa-envelope icon mt-1"></i>
+                          <p className="ml-3 mt-3">Contact</p>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
 
-                <div className= "menu-bottom navbar navbar-expand-sm  fixed-bottom" id="navbarNav">
+                <div className= "menu-bottom navbar navbar-expand-sm fixed-bottom" id="navbarNav"> {/* Navbar du bas*/}
                   <div>
                     <button className="btn btn-link" type="button" >
-                          <i class="fa fa-search fa-2x"></i>
+                          <i class="fa fa-search fa-2x"></i>           {/* icone recherche */}
                     </button>
                     </div>
 
-                    <div className="icon-bottom">
+                    <div className="icon-bottom">                      {/* icones de droite*/}
                       <ul className="navbar-nav">
                           <li className="nav-item active pr-5">
                               
