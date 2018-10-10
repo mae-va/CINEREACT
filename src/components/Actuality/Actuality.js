f28e531142800c8e543dd429d9bdcd93511b8b07
Matthieu Petit <matthieuwpetit@gmail.com>
2018-10-10 17:36:47 +0200

    Actuality temporaire def

--- src/components/Actuality/Actuality.js
+++ src/components/Actuality/Actuality.js
@@ -44,12 +44,12 @@ class Actuality extends Component {
                 <div className="container-overlay pl-0"></div>
                     <div className="container-fluid bloc_actuality pl-0 pr-0">
                         <div class="row actuality">
-                            <div className="col-lg-7 col-md-12 pl-0 pr-0 poster_column">
+                            <div className="col-lg-6 col-md-6 pl-0 pr-0 poster_column">
                                 <img className="img-fluid movie_poster" alt="movie_poster" src={`https://image.tmdb.org/t/p/original${this.state.movies.poster_path}`}/>
                             </div>
 
-                                <div className="col-lg-5 col-md-12 black">
-                                    <div className="row mb-3 pl-5 title">INTERSTELLAR</div>
+                                <div className="col-lg-6 col-md-6 black">
+                                    <div className="row mb-5 pl-5 title">TERRIFIANT!</div>
                                     <div className="row pl-5 year">
                                         <p>2014</p> </div>
                                     <div className="row pl-5 director">
@@ -57,9 +57,9 @@ class Actuality extends Component {
                            
 
                                     <div className="row pl-5 pb-3 casting">Matthhew Mac caunaughey, Bulbizarre, Pikaku, Jean-Pierre Css...</div>
-                                    <div className="row synopsis pb-4 pl-5 pr-5 mr-5mb-5 d-none d-lg-block">
+                                    <div className="row synopsis pb-5 pl-5 pr-5 mr-5mb-5 d-none d-lg-block">
                                         <p>Il etait une fois un gars vraizment biaeze r qui ecrviat une histoire à la con trs amuvaius. La terre etait condamner et lui ne chercahit qu'à écrite de fou...</p>
-                                        <button className="readMore ml-5">+</button>
+                                        <button className="readMore ml-3">+</button>
                                     </div>
                                     <div className="row favoritesRating">
                                         <i class="fa fa-heart pl-5 pr-5"></i>
