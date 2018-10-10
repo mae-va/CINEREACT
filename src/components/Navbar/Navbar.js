123676e9acd397478265f0f578a8b56d999b5153
Simon Philouze <contact@simonphilouze.fr>
2018-10-10 12:43:49 +0200

    Modif Simon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
 import {Link} from "react-router-dom";
 import './Navbar.css';
-import {Link} from "react-router-dom";
-
-
 
 class Navbar extends Component {
     constructor(props) {
         super(props);
     }
-
     render(){
         return (
           <div className="container-fluid">
             <nav className="navbar navbar-expand-md fixed-top"> {/* Navbar du haut*/}
             <Link exact to="/">
-                    <h1 class="logo-cine">CINE</h1>
-                    <h1 class="logo-react">REACT</h1> {/* Logo */}
+              <h1 class="logo-cine">CINE</h1>
+              <h1 class="logo-react">REACT</h1> {/* Logo */}
             </Link>
                 <div className="collapse navbar-collapse" > {/* icones de droite*/}
                   <div>
@@ -24,11 +20,10 @@ class Navbar extends Component {
                           <i class="fa fa-search fa-2x"></i>
                     </button>
                   </div>
-
                   <div>
                     <ul className="navbar-nav">
                       <li className="nav-item active pr-5">
-                        <Link to="/favoris" className="nav-link ml-3 mt-3">
+                        <Link to="/favoris" className="nav-link">
                           <i className="fa fa-heart icon"></i>
                           <p className="text-nav ml-3 mt-3">Favoris</p>
                         </Link>
@@ -42,15 +37,13 @@ class Navbar extends Component {
                     </ul>
                   </div>
                 </div>
-
                 <div className= "menu-bottom navbar navbar-expand-sm fixed-bottom" id="navbarNav"> {/* Navbar du bas*/}
                   <div>
                     <button className="btn btn-link" type="button" >
-                          <i class="fa fa-search fa-2x"></i>           {/* icone recherche */}
+                        <i class="fa fa-search fa-2x"></i>{/* icone recherche */}
                     </button>
                     </div>
-
-                    <div className="icon-bottom">                      {/* icones de droite*/}
+                    <div className="icon-bottom">{/* icones de droite*/}
                       <ul className="navbar-nav">
                           <li className="nav-item active pr-5">
                               
