561b93bba82c02a951cd651575768f735b9ae34a
Antoine Nourris <nourrisantoine44@gmail.com>
2018-10-16 17:46:15 +0200

    correction bug actuality + warnings + modal recherche et contact

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import {Link} from "react-router-dom";
 import './Navbar.css';
 import Search from '../Search/Search';
+import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+
 
 class Navbar extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+          modal : false
+        }
+
     }
+
+    toggle = () =>{
+      this.setState({ modal : !this.state.modal})
+    }
+
     render(){
         return (
           <div>
@@ -31,20 +43,30 @@ class Navbar extends Component {
                     </ul>
                   </div>
                 </div>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    <i className="fa fa-envelope icon"></i>
-                    <p className="text-nav ml-3 mt-3">CONTACT</p>
-                  </a>
+                <li className="nav-item">    
+                  <div className="nav-link">
+                    <i className="fa fa-envelope icon" onClick={this.toggle}></i>
+                    <p className="text-nav ml-3 mt-3" onClick={this.toggle}>CONTACT</p>
+                  </div>  
+                  <div>
+                    <Modal isOpen={this.state.modal} toggle={this.toggle} >
+                      <ModalHeader toggle={this.toggle} className="modal-contact">CONTACT</ModalHeader>
+                      <ModalBody className="modal-contact">
+                        <p>Names : Maéva Duran, Matthieu Petit, Tiphaine Deswartes, Antoine Nourris </p>
+                        <hr className="mx-5"/>
+                        <p>Email address :     Contact@simonphilouze.fr </p>
+                      </ModalBody>
+                    </Modal>
+  					      </div> 
                 </li>
                 <div className= "menu-bottom navbar navbar-expand-sm fixed-bottom" id="navbarNav"> {/* Navbar du bas*/}
                     <Search />
                   <div className="icon-bottom">                      {/* icones de droite*/}
                     <ul className="navbar-nav">
                       <li className="nav-item active">
-                        <a className="nav-link" href="#">
-                          <i className="fa fa-heart fa-1x icon"></i>
-                        </a>
+                        <Link to="/favoris" className="nav-link">
+                          <i className="text-danger fa fa-heart fa-1x icon"></i>
+                        </Link>
                       </li>
                     </ul>
                   </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/jquery/dist/jquery.min.js';
 import {BrowserRouter} from "react-router-dom";
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 
