56617a4ee86050986c06ffe994f34d8b90077d18
Maeva Duran <maeva.v.duran@gmail.com>
2018-10-17 15:59:27 +0200

    version mobile et ipad de la page actu

--- src/components/ReadMore/ReadMore.js
+++ src/components/ReadMore/ReadMore.js
@@ -19,23 +19,27 @@ class ReadMore extends Component {
     render (){
         const style = this.state.readMore ? {display: 'none'} : {};
         return (
-            <div className="container mobile-readmore mt-5" style={style}>
+            <div className="container mobile-readmore" style={style}>
                 <div className="col-sm-12 bloc-txt"> {/* affichage mobile*/}
-                    <div className="row-m m-title mb-3 mt-5">{this.props.title}</div>
-                    <div className="row-m">
+                    <div className="m-title mb-3">{this.props.title}</div>
+                    <div className="">
                         <p>{this.props.year} - David Lynch</p> 
                     </div>
-                    <div className="row-m m-casting">
-                        <em className="mb-3">Avec: Antoine Nourris, Tiphaine Deswartes, Maéva Duran, Matthieu Petit</em>
-                        <Rating className="rating-mobile mt-5 pt-5 mb-3" value="3" readonly/>
+                    <div className="m-casting">
+                        <em className="mb-3">Avec: Antoine Nourris, Tiphaine Deswartes, Maéva Duran, Matthieu Petit</em><br />
+                        <div className="rating-mobile mt-3 mb-3">
+                            <Rating value="3" readonly/>
+                        </div>
+                        
                     </div>
-                    <div className="row-m m-synopsis mt-3">
+                    <div className="m-synopsis mt-3">
                         <p>{this.props.synopsis}</p>
                     </div>
-                    <div className="row-m icons-bottom">
-                        <i className="fa fa-heart fa-2x mr-5 ml-2" onClick={this.handleClick}></i>
-                        <i class="fa fa-times fa-2x mr-5" onClick={this.closeReadMore}></i>
-                    </div>
+                    
+                </div>
+                <div className="col-sm-12 icons-bottom mt-5">
+                    <i className="fa fa-heart fa-2x" onClick={this.handleClick}></i>
+                    <i class="fa fa-times fa-2x" onClick={this.closeReadMore}></i>
                 </div>
             </div>
         )
