38e9e6ce8e6a89893776042fbe843d69b3c1c5a2
Maeva Duran <maeva.v.duran@gmail.com>
2018-10-19 16:27:13 +0200

    ajout condition favoris vides

--- src/components/Favoris/Favoris.js
+++ src/components/Favoris/Favoris.js
@@ -39,63 +39,46 @@ class Favoris extends Component {
   }
 
   render() {
-    return (
-      <div className="row top">
-        {this.state.movies.map((movie,index) =>{
-          return(
-            <Col md="6 nopadding">
-              <Card key={index}>
-              <Row>
-                <Col md="5">
-                  <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} class="movie-poster-favoris" ></img>
-                </Col>
-                <Col md="7" className="actuality-desktop-description">
-                  <CardBody>
-                    <CardTitle className="display-4 text-uppercase ">{movie.title}</CardTitle>
-                    <CardText className="mb-4">
-                      <i className= {`fa fa-heart pull-right mr-3 mt-2`} onClick={ () => {this.removeMovie(movie.id);}}></i>
-                      <Rating value={movie.vote_average} color="#f4dc42" weight="24" readonly/>
-                    </CardText>
-                    <CardSubtitle className="lead text-white mb-2 ">
-                      {movie.release_date} - {movie.director}
-                    </CardSubtitle>
-                    <CardText className="font-weight-bold font-italic">{movie.casting}</CardText>
-                    <CardText className="mt-4 text-description-favoris">{movie.overview}</CardText>
-                  </CardBody>
-                </Col>
-              </Row>
-            </Card>
-            </Col>
-            // <div key={index} className="col-lg-6 col-md-12 col-sm-12 col-xs-12 col-12 p-0">
-            //   <div className="card mt-5 mx-5">
-            //     <div className="row h-100 ">
-            //       <div className="col-lg-5 col-md-6 col-sm-6 col-xs-6 col-6 ajust-height p-0 poster-like">
-            //         <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.title} className="fav-image h-100 w-100"/>
-            //       </div>
-            //       <div className="col-lg-7 col-md-6 col-sm-6 col-xs-6 col-6 ajust-height p-0 h-100">
-            //         <div className="card-block p-5" id="position">
-            //           <h4 className="card-title">{movie.title}</h4>
-            //           <p className="card-text"> {movie.release_date} {movie.director}</p>
-            //           <p className="card-text d-none d-sm-none d-md-block d-lg-block">{movie.casting}</p>
-            //           <div className="sidebar-box d-none d-sm-none d-md-block d-lg-block">
-            //             <p className="card-text">{movie.overview}</p>
-            //           </div>
-            //           <div className="row favoritesRating pb-4 pr-5 mr-0 w-100">
-            //             <i className="fa fa-heart pt-1 pl-3 pr-5 coeur" onClick={() => {this.removeMovie(movie.id);}}></i>
-            //             <Rating value={movie.vote_average} color="#f4dc42" weight="24" readonly/>
-            //             </div>
-            //             <div className= "cast">
-            //             <p>Brad Pitt , Nicole Kidman , Jim Carrey ...</p>
-            //             </div>
-            //           </div>
-            //         </div>
-            //       </div>
-            //     </div>
-            //   </div>
-          )}
-        )}
-      </div>
-    )
+    if(this.state.movies.length === 0){
+      return(
+        <p className="fav-phrase">Here, you can add your favorite movies.</p>
+      )
+    }
+    else {
+      return (
+        <div className="row top">
+          {this.state.movies.map((movie,index) =>{
+            return(
+              <Col md="6 nopadding">
+                <Card key={index}>
+                <Row>
+                  <Col md="5">
+                    <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} class="movie-poster-favoris" ></img>
+                  </Col>
+                  <Col md="7" className="actuality-desktop-description">
+                    <CardBody>
+                      <CardTitle className="display-4 text-uppercase ">{movie.title}</CardTitle>
+                      <CardText className="mb-4">
+                        <i className= {`fa fa-heart pull-right mr-3 mt-2`} onClick={ () => {this.removeMovie(movie.id);}}></i>
+                        <Rating value={movie.vote_average} color="#f4dc42" weight="24" readonly/>
+                      </CardText>
+                      <CardSubtitle className="lead text-white mb-2 ">
+                        {movie.release_date} - {movie.director}
+                      </CardSubtitle>
+                      <CardText className="font-weight-bold font-italic">{movie.casting}</CardText>
+                      <CardText className="mt-4 text-description-favoris">{movie.overview}</CardText>
+                    </CardBody>
+                  </Col>
+                </Row>
+                </Card>
+              </Col>
+            )
+          })
+        }
+        </div>
+      );
+    }
   }
 }
+
 export default Favoris;
